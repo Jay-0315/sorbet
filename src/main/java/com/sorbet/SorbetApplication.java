9208684ca551a58e33afeb53397babbaa45c6e4f
Jay <huny000315@gmail.com>
2025-07-14 11:10:53 +0900

    initial commit

---
+++ src/main/java/com/sorbet/SorbetApplication.java
@@ -0,0 +1,13 @@
+package com.sorbet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorbetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SorbetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sorbet/SorbetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sorbet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorbetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
