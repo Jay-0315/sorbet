87366fcecdd10b6e731536550bdc82d070bcbeee
lee <leesh1999@naver.com>
2025-07-18 09:53:08 +0900

    게시판 화면

--- src/main/java/com/sorbet/consulting/sorbet.java
+++ src/main/java/com/sorbet/consulting/sorbet.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConsultingApplication {
+public class sorbet {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConsultingApplication.class, args);
+        SpringApplication.run(sorbet.class, args);
     }
 
 }
